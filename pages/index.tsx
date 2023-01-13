058db80af1df6b492bd251d540a0895c362b9819
Efiamotu-1 <musahabeebefy10@gmail.com>
2023-01-13 12:19:28 +0100

    socials only

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,27 @@ import { GetStaticProps } from 'next'
 import Head from 'next/head'
 // import About from '../components/About'
 // import Contact from '../components/Contact'
-// import Header from '../components/Header'
+import Header from '../components/Header'
 // import Hero from '../components/Hero'
 // import Projects from '../components/Projects'
 // import Skills from '../components/Skills'
 // import WorkExperience from '../components/WorkExperience'
 import { Experience, PageInfo, Project, Skill, Social } from '../typings'
-import { fetchExperiences } from '../utils/fetchExperiences'
-import { fetchPageInfo } from '../utils/fetchPageInfo'
-import { fetchProjects } from '../utils/fetchProjects'
-import { fetchSkills } from '../utils/fetchSkills'
+// import { fetchExperiences } from '../utils/fetchExperiences'
+// import { fetchPageInfo } from '../utils/fetchPageInfo'
+// import { fetchProjects } from '../utils/fetchProjects'
+// import { fetchSkills } from '../utils/fetchSkills'
 import { fetchSocials } from '../utils/fetchSocials'
 
 type Props = {
-  pageInfo: PageInfo;
-  experiences: Experience[];
-  skills: Skill[];
-  projects: Project[];
+  // pageInfo: PageInfo;
+  // experiences: Experience[];
+  // skills: Skill[];
+  // projects: Project[];
   socials: Social[]
 }
 
-export default function Home() {
+export default function Home({socials}: Props) {
 
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll 
@@ -33,7 +33,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
     <div className='text-center mt-20'>Hello World welcome to my revised portfolio</div>
-    {/* <Header socials={socials}/> */}
+    <Header socials={socials}/>
     {/* Hero  */}
     {/* <section id='hero'>
       <Hero pageInfo={pageInfo}/>
@@ -72,19 +72,19 @@ export default function Home() {
 }
 
 export const  getStaticProps: GetStaticProps<Props> = async () => {
-  const pageInfo: PageInfo = await fetchPageInfo();
-  const experiences: Experience[] = await fetchExperiences();
-  const skills: Skill[] = await fetchSkills();
-  const projects: Project[] = await fetchProjects();
+  // const pageInfo: PageInfo = await fetchPageInfo();
+  // const experiences: Experience[] = await fetchExperiences();
+  // const skills: Skill[] = await fetchSkills();
+  // const projects: Project[] = await fetchProjects();
   const socials: Social[] = await fetchSocials();
 
   return {
     props: {
-      pageInfo,
-      experiences,
-      skills,
-      projects,
-      socials,
+      // pageInfo,
+      // experiences,
+      // skills,
+      // projects,
+      socials
     },
 
     // Next.js will attempt to re-generate the page
